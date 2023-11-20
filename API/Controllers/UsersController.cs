758ef4abe3eb501418e6cb90b98097961fdb2c55
Lee <leesheppard2404@gmail.com>
2023-11-20 16:50:49 +0100

    feat: Add UsersController with GetUsers and GetUser methods

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+// controller is a placeholder for the name of the controller
+// in this case, the name of the controller is UsersController
+// so the route will be https://localhost:5001/api/users
+public class UsersController : ControllerBase
+{
+  private readonly DataContext _context;
+
+  public UsersController(DataContext context)
+  {
+    _context = context;
+  }
+
+  [HttpGet]
+  public ActionResult<IEnumerable<AppUser>> GetUsers()
+  {
+    var users = _context.Users.ToList();
+    return users;
+  }
+
+  [HttpGet("{id}")] // https://localhost:5001/api/users/3
+  public ActionResult<AppUser> GetUser(int id)
+  {
+    var user = _context.Users.Find(id);
+    return user;
+  }
+}
