1dad29f1164f899c0da094e1412866199aed867f
Lee <leesheppard2404@gmail.com>
2023-11-20 13:26:21 +0100

    refactor: Add DataContext as a service

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Here we are adding teh DataContext as a service
+// We will need access to the class to access and update the database
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
