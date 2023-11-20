d28a9a07ab5ec758526bd6b0b3d9a4eaefb127cc
Lee <leesheppard2404@gmail.com>
2023-11-20 16:50:27 +0100

    chore: Add controller and authorization middleware

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
+
 // Here we are adding teh DataContext as a service
 // We will need access to the class to access and update the database
 builder.Services.AddDbContext<DataContext>(opt =>
@@ -16,5 +19,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
