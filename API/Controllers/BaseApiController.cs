4c9b4782efff0f2ac2552bb84e8e8c270cdb3300
Lee <leesheppard2404@gmail.com>
2023-11-21 15:32:05 +0100

    refactor: Add BaseApiController as a base class for UsersController

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  // controller is a placeholder for the name of the controller
+  // in this case, the name of the controller is UsersController
+  // so the route will be https://localhost:5001/api/users
+  public class BaseApiController : ControllerBase
+  {
+  }
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,12 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-// controller is a placeholder for the name of the controller
-// in this case, the name of the controller is UsersController
-// so the route will be https://localhost:5001/api/users
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
   private readonly DataContext _context;
 
