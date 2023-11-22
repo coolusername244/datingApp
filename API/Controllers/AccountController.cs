33d543fe771157cdcda51cbf33eac8f3b987c5f3
Lee <leesheppard2404@gmail.com>
2023-11-22 11:39:57 +0100

    refactor: changed register function to use DTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -14,22 +16,29 @@ namespace API.Controllers
       _context = context;
     }
     [HttpPost("register")] //POST: https://localhost:5001/api/account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+
+      if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
       // using 'using' statement to dispose of the HMACSHA512 object after it's used
       // cleaning up memory
       using var hmac = new HMACSHA512();
 
       var user = new AppUser
       {
-        UserName = username,
-        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+        UserName = registerDto.Username.ToLower(),
+        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
         PasswordSalt = hmac.Key
       };
       _context.Users.Add(user);
       await _context.SaveChangesAsync();
 
       return user;
+    }
+    private async Task<bool> UserExists(string username)
+    {
+      return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
   }
 }
