33c9ac1cec1d57efb8a0e9c9ad727621038526ae
Lee <leesheppard2404@gmail.com>
2023-11-25 14:53:58 +0100

    chore: Add UserRepository and IUserRepository interfaces

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+  public interface IUserRepository
+  {
+    void Update(AppUser user);
+    Task<bool> SavellAsync();
+    Task<IEnumerable<AppUser>> GetUserAsync();
+    Task<AppUser> GetUserByIdAsync(int id);
+    Task<AppUser> GetUserByUsernameAsync(string username);
+  }
+}
+
